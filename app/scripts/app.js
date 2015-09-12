17eb6a396e3cbbb90ab2b33539ae0484a2e8906b
Loic <talhouar.loic@gmail.com>
2015-09-12 15:59:42 -0500

    Adding content

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,13 +21,8 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
+        controller: 'mainController',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
